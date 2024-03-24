b017425be20c7f7e334eeb72893d624397414415
Mussry0Taha <148034001+Mussry0Taha@users.noreply.github.com>
2024-03-24 21:06:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function search() {
+    const searchTerm = document.getElementById("searchInput").value;
+    // Perform search functionality based on searchTerm
+    alert("Searching for: " + searchTerm);
+  }
+  
